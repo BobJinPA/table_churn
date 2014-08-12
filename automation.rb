751cf0ece85499f10f937119c8bf9d5a9389f3f6
Bob Jones <bjones@chathamfinancial.com>
2014-08-12 12:38:05 -0400

    refactored with trs method and use of benchmark gem

--- automation.rb
+++ automation.rb
@@ -1,26 +1,43 @@
 require 'watir-webdriver'
 require_relative "table"
+require 'benchmark'
 
-b = Watir::Browser.new :chrome
-
+b = Watir::Browser.new :ie
 b.goto 'http://localhost:4567/sample'
-
 sample = b.table(:id => 'sample')
 
-(0...sample.trs.size).each do |row|
-  (0...sample.row.cells.length).each do |cell|
-    puts sample[row][cell].text
+def table_row(sample)
+  (0...sample.trs.size).each do |row|
+    (0...sample.row.cells.length).each do |cell|
+      sample[row][cell].text
+    end
   end
 end
 
-puts '-----------'
 
-sample_array = sample.to_a
-
-(0...sample_array.size).each do |row|
-  (0...sample_array[0].size).each do |cell|
-    puts sample_array[row][cell]
+def table_trs(sample)
+  sample.trs.each do |row|
+    row.cells.each do |cell|
+      cell.text
+    end
   end
+end
+
+
+def table_noko(sample)
+  sample_array = sample.to_a
+  (0...sample_array.size).each do |row|
+    (0...sample_array[0].size).each do |cell|
+      sample_array[row][cell]
+    end
+  end
+end
+
+
+Benchmark.bm(7) do |x|
+  x.report("table_row:")   { table_row(sample)}
+  x.report("table_trs:") { table_trs(sample)}
+  x.report("table_noko:")  { table_noko(sample)}
 end
 
 b.close
